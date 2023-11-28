02f2b163e2b9a0e2f8e8f2749ac00534152e9666
semenovrv <30365775+semenovrv@users.noreply.github.com>
2023-11-28 09:12:16 +0300

    Add files via upload

--- src/napi.chem.h
+++ src/napi.chem.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <napi.h>
-#include "simple_molecule.h"
 #include "edited_molecule.h"
 
 #define SVG_MODE 2
@@ -13,18 +12,6 @@ typedef struct{
     int height=150;
     bool numerationDraw=false;
 } Search_Mode;
-
-class Otrep : public Napi::ObjectWrap<Otrep>
-{
-public:
-    Otrep(const Napi::CallbackInfo&);
-    Napi::Value Greet(const Napi::CallbackInfo&);
-
-    static Napi::Function GetClass(Napi::Env);
-
-private:
-    std::string _greeterName;
-};
 
 class QueryMol : public Napi::ObjectWrap<QueryMol>
 {
@@ -42,23 +29,3 @@ private:
 	Search_Mode _mode;
 };
 
-class SMol : public Napi::ObjectWrap<SMol>
-{
-public:
-    SMol(const Napi::CallbackInfo&);
-//	Napi::Value handle(const Napi::CallbackInfo&);
-	Napi::Value nAtoms(const Napi::CallbackInfo&);
-	Napi::Value nBonds(const Napi::CallbackInfo&);
-	Napi::Value readMolfile(const Napi::CallbackInfo&);
-	Napi::Value readMolfilePromise(const Napi::CallbackInfo&);
-	Napi::Value prepareQuery(const Napi::CallbackInfo&);
-	Napi::Value match(const Napi::CallbackInfo&);
-	Napi::Value saveSVG(const Napi::CallbackInfo&);
-
-    static Napi::Function GetClass(Napi::Env);
-
-private:
-	Search_Mode _mode;
-	MolStruct::TSimpleMolecule _smol;
-	MolStruct::TEditedMolecule _query;
-};
